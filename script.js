65e0bf437039fff4c2f827f4efc331d980fa8e39
Penkcuy <50241044+Penkcuy@users.noreply.github.com>
2026-04-23 10:52:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
 AOS.init();
 
+// nama tamu
 const params = new URLSearchParams(window.location.search);
 const nama = params.get("to");
 
 document.getElementById("namaTamu").innerText =
-  nama ? "Kepada: " + nama : "Tamu Undangan";
+  nama ? "Kepada: " + nama : "";
 
-function bukaUndangan() {
-  const cover = document.getElementById("cover");
-
-  cover.style.transition = "1s";
-  cover.style.opacity = "0";
-
-  setTimeout(() => {
-    cover.style.display = "none";
-    document.getElementById("content").classList.remove("hidden");
-  }, 1000);
+// buka undangan
+function start(){
+  document.getElementById("opening").style.display="none";
 
   const musik = document.getElementById("musik");
   musik.volume = 0.3;
   musik.play();
-
-  document.querySelector(".music-btn").innerText = "🔊";
 }
 
-function toggleMusic() {
+// toggle musik
+function toggleMusic(){
   const musik = document.getElementById("musik");
   const btn = document.querySelector(".music-btn");
 
-  if (musik.paused) {
+  if(musik.paused){
     musik.play();
-    btn.innerText = "🔊";
+    btn.innerText="🔊";
   } else {
     musik.pause();
-    btn.innerText = "🔇";
+    btn.innerText="🔇";
   }
 }
 
-const target = new Date("May 10, 2026 10:00:00").getTime();
+// countdown
+const target = new Date("May 10, 2026").getTime();
 
-setInterval(() => {
+setInterval(()=>{
   const now = new Date().getTime();
-  const selisih = target - now;
+  const d = target - now;
 
-  const hari = Math.floor(selisih / (1000*60*60*24));
-  const jam = Math.floor((selisih / (1000*60*60)) % 24);
-  const menit = Math.floor((selisih / (1000*60)) % 60);
+  const hari = Math.floor(d/(1000*60*60*24));
 
-  document.getElementById("countdown").innerText =
-    `${hari} hari ${jam} jam ${menit} menit`;
-}, 1000);
+  document.getElementById("countdown").innerText = hari + " hari";
+},1000);
