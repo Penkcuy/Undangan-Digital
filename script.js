f0dec1b061b4d28e185146afadcd0907b05d6e82
Penkcuy <50241044+Penkcuy@users.noreply.github.com>
2026-04-23 09:47:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-// Ambil nama dari URL
+AOS.init();
+
+// Nama tamu
 const params = new URLSearchParams(window.location.search);
 const nama = params.get("to");
 
 document.getElementById("namaTamu").innerText =
   nama ? "Kepada: " + nama : "Tamu Undangan";
 
-// buka undangan + play musik
+// buka undangan
 function bukaUndangan() {
-  document.querySelector(".cover").style.display = "none";
+  document.getElementById("cover").style.display = "none";
   document.getElementById("content").classList.remove("hidden");
 
-  document.getElementById("musik").play();
+  const musik = document.getElementById("musik");
+  musik.volume = 0.3;
+  musik.play();
 }
 
-// countdown
-const targetDate = new Date("May 10, 2026 10:00:00").getTime();
+// toggle musik
+function toggleMusic() {
+  const musik = document.getElementById("musik");
+
+  if (musik.paused) {
+    musik.play();
+  } else {
+    musik.pause();
+  }
+}
+
+// countdown detail
+const target = new Date("May 10, 2026 10:00:00").getTime();
 
 setInterval(() => {
   const now = new Date().getTime();
-  const selisih = targetDate - now;
+  const selisih = target - now;
 
-  const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+  const hari = Math.floor(selisih / (1000*60*60*24));
+  const jam = Math.floor((selisih / (1000*60*60)) % 24);
+  const menit = Math.floor((selisih / (1000*60)) % 60);
 
   document.getElementById("countdown").innerText =
-    hari + " hari lagi";
+    `${hari} hari ${jam} jam ${menit} menit`;
 }, 1000);
-
-document.getElementById("musik").volume = 0.3;
